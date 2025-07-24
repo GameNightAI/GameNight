06352af53e56f0b70cc850b4f30fec8315795cad
BaronSmoki <163218003+BaronSmoki@users.noreply.github.com>
2025-07-24 15:41:11 -0400

    Updated FilterGameModal.tsx

--- components/FilterGameModal.tsx
+++ components/FilterGameModal.tsx
@@ -31,7 +31,7 @@ export const FilterGameModal: React.FC<FilterGameModalProps> = ({
   const playerOptions = Array.from({ length: 14 }, (_, i) => String(i + 1)).concat(['15+']);
   const timeOptions = ['30', '60', '90', '120+'];
   const ageOptions = ['6+', '8+', '10+', '12+', '14+', '16+'];
-  const typeOptions = [{any: 'Any'}, {cooperative: 'Cooperative'}, {competitive: 'Competitive'}, {teambased: 'Team-based'}];
+  const typeOptions = ['Any', 'Competitive', 'Cooperative', 'Team-based'].map((_) => {return {value: _, label: _}});
   const complexityOptions = ['Light', 'Medium Light', 'Medium', 'Medium Heavy', 'Heavy'];
 
   const handleSearch = () => {
