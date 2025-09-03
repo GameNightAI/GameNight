cf64bb0ac4943667a811517d700a472353d15e2e
Brian Somok <bsomok@gmail.com>
2025-09-03 07:36:49 -0400

    Semantic improvement (games -> exp_games)

--- supabase/views/collections_games_expansions.sql
+++ supabase/views/collections_games_expansions.sql
@@ -4,11 +4,11 @@ create or replace view collections_games_expansions with (security_invoker = on)
   select
     base.*,
     expansions.expansion_id,
-    games.name as expansion_name,
-    games.min_players as expansion_min_players,
-    games.max_players as expansion_max_players,
+    exp_games.name as expansion_name,
+    exp_games.min_players as expansion_min_players,
+    exp_games.max_players as expansion_max_players,
     coalesce(exp.is_expansion, false) as is_expansion_owned,
-    games.thumbnail as expansion_thumbnail
+    exp_games.thumbnail as expansion_thumbnail
   from
     collections_games as base
     left join expansions
@@ -16,15 +16,15 @@ create or replace view collections_games_expansions with (security_invoker = on)
     left join collections_games as exp
       on expansions.expansion_id = exp.bgg_game_id
       and base.user_id = exp.user_id
-    left join games
-      on expansions.expansion_id = games.id
+    left join games as exp_games
+      on expansions.expansion_id = exp_games.id
   where
     base.is_expansion = false
     and (
       exp.is_expansion = true /* is_expansion_owned */
       /* Show all unowned expansions, as long as they're not promos or fan expansions. */
       or not (
-        coalesce(games.boardgamefamily, '') like '%Promotional:%'
-        or coalesce(games.boardgamecategory, '') like '%Fan Expansion%'
+        coalesce(exp_games.boardgamefamily, '') like '%Promotional:%'
+        or coalesce(exp_games.boardgamecategory, '') like '%Fan Expansion%'
       )
     )
