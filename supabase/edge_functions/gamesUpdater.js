c5e7ba51092ab2981ca74bcf77ea7d9e0199f2c9
Brian Somok <bsomok@gmail.com>
2025-09-21 20:07:43 -0400

    Lots of progress!

--- supabase/edge_functions/gamesUpdater.js
+++ supabase/edge_functions/gamesUpdater.js
@@ -7,10 +7,12 @@ import { asyncBatch } from 'iter-tools';
 const LOGIN_URL = 'https://boardgamegeek.com/login/api/v1';
 const BGG_CSV_URL = 'https://boardgamegeek.com/data_dumps/bg_ranks';
 const SLEEP_TIME = 5 // seconds to wait before retrying BGG API request
+const DASH = '–' // NOT the hyphen character on the keyboard
+const TAXONOMY_DELIMITER = '|';
 
 const getZipUrl = async () => {
   
-  const username = process.env.BGG_USERNAME
+  const username = process.env.BGG_USERNAME;
   console.log(`Logging ${username} in to BGG...`);
   const loginResponse = await fetch(
     LOGIN_URL, {
@@ -18,10 +20,15 @@ const getZipUrl = async () => {
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify({ credentials: {
         username: username,
-        password: process.env.BGG_PASSWORD
+        password: process.env.BGG_PASSWORD,
       }}),
     }
   );
+  if (loginResponse.ok) {
+    console.log('Successfully logged in.');
+  } else {
+    throw new Error(`Failed to log in: ${loginResponse.status} - ${loginResponse.statusText}`);
+  }
   const cookie = await loginResponse.headers.getSetCookie();
 
   console.log(`Fetching BGG zip URL from ${BGG_CSV_URL}...`);
@@ -37,7 +44,7 @@ const getZipUrl = async () => {
   const doc = parser.parseFromString(await html, 'text/html');
   const hyperlink = await doc
     .getElementById('maincontent')
-    .getElementsByTagName('a')[0]
+    .getElementsByTagName('a')[0];
   
   const url = await hyperlink.getAttribute('href');
   const filename = await hyperlink.getAttribute('download');
@@ -45,41 +52,101 @@ const getZipUrl = async () => {
   return [url, filename];
 }
 
-const parseXml = (text) => {
-  
-  const parser = new XMLParser({
-    ignoreAttributes: false,
-    // attributeNamePrefix: '',
-    // textNodeName: 'value',
-  });
+const hasTaxonomy = (game, type, value) => (
+  game.link.some(link =>
+    link.type === type && link.value === value
+  )
+);
+
+const parser = new XMLParser({
+  ignoreAttributes: false,
+  attributeNamePrefix: '',
+  // textNodeName: 'value',
+});
+
+const parseXml = function* (text) {
   
   let games = parser.parse(text).items.item;
+  // console.log(games);
   // Handle edge case where the final batch of games is a single item
   if (!Array.isArray(games)) {
     games = [games];
   }
-  for (game of games) {
+  for (const game of games) {
     const row = {};
-    for (poll of game.poll) {
+    for (const poll of game.poll) {
       if (poll.name === 'suggested_playerage') {
         // Avoid division by zero
-        if poll.totalvotes == '0' {
+        if (poll.totalvotes === '0') {
           row.suggested_playerage = null;
         } else {
           let ageSum = 0;
           let voteSum = 0;
-          for (result in poll.results.result) {
+          console.log(poll.results.result);
+          for (const result in poll.results) {
+            console.log(result);
             const age = parseInt(result.value);
-            const voteCount = parseInt(
+            const voteCount = parseInt(result.numvotes);
+            ageSum += age * voteCount;
+            voteSum += voteCount;
+            console.log(ageSum, voteSum);
+          }
+          row.suggested_playerage = ageSum / voteSum; // weighted average
+        }
+      }
+    }
+    let summaries = game['poll-summary'];
+    if (!Array.isArray(summaries)) {
+      summaries = [summaries];
+    }
+    for (const summary of summaries) {
+      if (summary.name === 'suggested_numplayers') {
+        for (const result in summary.result) {
+          if (result.name === 'bestwith') {
+            row.best_players = null; // TODO
+          } else if (result.name === 'recommmendedwith') {
+            row.rec_players = null; // TODO
           }
         }
       }
     }
+    row.expansions = [];
+    // Don't get expansions of expansions, just of base games
+    if (game.type === 'boardgame') {
+      for (const link of game.link) {
+        if (link.type === 'boardgameexpansion') {
+          row.expansions.push(link.id);
+        }
+      }
+    }
+    row.id = game.id;
+    // NULL out 0 for filtering purposes (0 means no value)
+    row.minplaytime = parseInt(game.minplaytime.value) || null;
+    row.maxplaytime = parseInt(game.maxplaytime.value) || null;
+    row.playing_time = parseInt(game.playingtime.value) || null;
+    row.min_players = parseInt(game.minplayers.value) || null;
+    row.max_players = parseInt(game.maxplayers.value) || null;
+    row.min_age = parseInt(game.minage.value) || null;
+    row.image_url = game.image;
+    row.thumbnail = game.thumbnail;
+    // NULL out complexity=0 for filtering purposes, and in case we ever decide to do some math (0 means no votes)
+    row.complexity = parseFloat(game.statistics.ratings.averageweight.value) || null;
+    row.description = null; // Leaving blank until we have more database storage
+    row.is_cooperative = hasTaxonomy(game, 'boardgamemechanic', 'Cooperative Game');
+    row.is_teambased = hasTaxonomy(game, 'boardgamemechanic', 'Team-Based Game');
+    // BGG taxonomy
+    for (const type of ['boardgamecategory', 'boardgamemechanic', 'boardgamefamily']) {
+      row[type] = game.link
+        .filter(link => link.type === type)
+        .map(link => link.value)
+        .join(TAXONOMY_DELIMITER);
+    }
+    console.log(row);
+    yield row;
   }
-  
 }
 
-const main = () => {
+const main = async () => {
 
   const [zipUrl, zipFilename] = await getZipUrl();
 
@@ -103,11 +170,13 @@ const main = () => {
   });
 
   let gameCount = 0;
-  console.log('Processing boardgames_ranks.csv...');
   // Make API calls in batches of 20 games at a time
   for await (const batch of await asyncBatch(20, parser)) {
+    if (gameCount === 0) {
+      console.log('Processing boardgames_ranks.csv...');
+    }
     const games = new Map();
-    for await (let row of batch) {
+    for await (const row of batch) {
       games.set(row.id, row);
       // We want 0 to show up as NULL in the database for sorting/filtering purposes
       for (const col of ['average', 'bayesaverage', 'rank', 'yearpublished']) {
@@ -115,16 +184,18 @@ const main = () => {
           row[col] = null;
         }
       }
-    }  
+    }
     
     const ids = Array.from(games.keys()).join();
     const url = `https://boardgamegeek.com/xmlapi2/thing?id=${ids}&stats=1`;
-    // let response;
+    console.log(url);
+    let response;
     
     while (1) {
-      const response = await fetch(url);
+      response = await fetch(url);
       const status = `${response.status} - ${response.statusText}`;
       if (response.ok) {
+        // console.log(response)
         break;
       } else if ([
         429, // Too many requests
@@ -139,6 +210,9 @@ const main = () => {
     
     let xmlText = await response.text();
     // console.log(xmlText);
+    for await (const row of parseXml(xmlText)) {
+      gameCount++;
+    }
   }
 };
 
