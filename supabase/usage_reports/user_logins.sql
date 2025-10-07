f9d2e428b76409260edfa9380fd585dbab833fef
Brian Somok <bsomok@gmail.com>
2025-10-07 00:22:18 -0400

    Removed yada yada

--- supabase/usage_reports/user_logins.sql
+++ supabase/usage_reports/user_logins.sql
@@ -5,13 +5,6 @@ from auth.audit_log_entries
 where
   payload ->> 'action' = 'login'
   and payload ->> 'actor_username' not in (
-    'bsomok@gmail.com',
-    'bsomok2@gmail.com',
-    'joshacain@gmail.com',
-    'kevin.wand@gmail.com',
-    'gamenightaidev@gmail.com',
-    'gamenyteapp@gmail.com',
-    'klackapp@gmail.com',
-    'hortesque@gmail.com'
+    'fakename@gmail.com'
   )
 order by created_at desc
