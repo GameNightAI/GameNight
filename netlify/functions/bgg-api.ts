f9a62137cf733e142d9e04070ee28ce6ef96f004
Brian Somok <bsomok@gmail.com>
2025-10-27 20:40:20 -0400

    Trying to fix netlify function issue

--- netlify/functions/bgg-api.ts
+++ netlify/functions/bgg-api.ts
@@ -1,6 +1,6 @@
 import { Config, Context, Handler } from '@netlify/functions';
 
-export default handler: Handler = async (req: Request, context: Context) => {
+export const handler: Handler = async (req: Request, context: Context) => {
   // const { command } = context.params;
   const apiKey = Netlify.env.get('BGG_API_AUTH_TOKEN');
   const urlSplit = req.url.split('/.netlify/functions/bgg-api/');
