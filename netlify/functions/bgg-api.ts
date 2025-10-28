90576b2c87bb2525d1fac33f94f6834d785aa899
Brian Somok <bsomok@gmail.com>
2025-10-28 02:51:57 -0400

    Trying to fix bgg-api.ts

--- netlify/functions/bgg-api.ts
+++ netlify/functions/bgg-api.ts
@@ -4,11 +4,17 @@ export const handler: Handler = async (req: Request, context: Context) => {
   // const { command } = context.params;
   console.log(req);
   const apiKey = process.env.BGG_API_AUTH_TOKEN;
-  const urlSplit = req.url.split('/.netlify/functions/bgg-api/');
+  const urlSplit = req.rawUrl.split('/.netlify/functions/bgg-api/');
   const apiString = urlSplit[urlSplit.length - 1];
-  req.url = `https://boardgamegeek.com/xmlapi2/${apiString}`;
-  req.headers.append('Authorization', `Bearer ${apiKey}`);
-  return await fetch(req);
+  // req.url = `https://boardgamegeek.com/xmlapi2/${apiString}`;
+  // req.headers.append('Authorization', `Bearer ${apiKey}`);
+  return await fetch(
+    `https://boardgamegeek.com/xmlapi2/${apiString}`, {
+    headers: {
+      ...req.headers,
+      Authorization: `Bearer ${BGG_API_AUTH_TOKEN}`,
+    }},
+  );
 };
 
 export const config: Config = {
