cd73bab0d6ecb3b692c933e54b2a1e6b2aa584d7
Brian Somok <bsomok@gmail.com>
2025-10-27 20:57:36 -0400

    Using process.env

--- netlify/functions/bgg-api.ts
+++ netlify/functions/bgg-api.ts
@@ -2,7 +2,7 @@ import { Config, Context, Handler } from '@netlify/functions';
 
 export const handler: Handler = async (req: Request, context: Context) => {
   // const { command } = context.params;
-  const apiKey = Netlify.env.get('BGG_API_AUTH_TOKEN');
+  const apiKey = process.env.BGG_API_AUTH_TOKEN;
   const urlSplit = req.url.split('/.netlify/functions/bgg-api/');
   const apiString = urlSplit[urlSplit.length - 1];
   req.url = `https://boardgamegeek.com/xmlapi2/${apiString}`;
@@ -10,6 +10,6 @@ export const handler: Handler = async (req: Request, context: Context) => {
   return await fetch(req);
 };
 
-// export const config: Config = {
-//   path: "/bgg-api/:command"
-// };
+export const config: Config = {
+  path: "/bgg-api/:command"
+};
