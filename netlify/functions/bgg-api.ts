57993009f5c4e7de56c923368357609bb5969a22
Brian Somok <bsomok@gmail.com>
2025-10-28 11:53:04 -0400

    Updated to use Handler types

--- netlify/functions/bgg-api.ts
+++ netlify/functions/bgg-api.ts
@@ -1,27 +1,28 @@
-import { Config, Context, Handler } from '@netlify/functions';
+import { Config, HandlerContext, Handler, HandlerEvent } from '@netlify/functions';
 
-export const handler: Handler = async (req: Request, context: Context) => {
+export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   // const { command } = context.params;
-  console.log(req);
+  console.log(event);
   const apiKey = process.env.BGG_API_AUTH_TOKEN;
-  const urlSplit = req.rawUrl.split('/.netlify/functions/bgg-api/');
+  const urlSplit = event.rawUrl.split('/.netlify/functions/bgg-api/');
   const apiString = urlSplit[urlSplit.length - 1];
   // req.url = `https://boardgamegeek.com/xmlapi2/${apiString}`;
   // req.headers.append('Authorization', `Bearer ${apiKey}`);
   const response = await fetch(
     `https://boardgamegeek.com/xmlapi2/${apiString}`, {
     headers: {
-      ...req.headers,
+      ...event.headers,
       Authorization: `Bearer ${apiKey}`,
     }},
   );
   console.log(response);
   return {
-    ...response,
+    // ...response,
     statusCode: response.status,
+    body: response.text(),
   };
 };
 
-export const config: Config = {
-  path: "/bgg-api/:command"
-};
+// export const config: Config = {
+//   path: "/bgg-api/:command"
+// };
