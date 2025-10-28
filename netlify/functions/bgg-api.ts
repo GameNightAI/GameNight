2cd033432e9c3d55f19451c9fb6ccf9ec7a72be9
Brian Somok <bsomok@gmail.com>
2025-10-28 12:12:44 -0400

    Cleaned up bgg-api.ts a bit

--- netlify/functions/bgg-api.ts
+++ netlify/functions/bgg-api.ts
@@ -1,13 +1,11 @@
-import { stream, Config, HandlerContext, Handler, HandlerEvent } from '@netlify/functions';
+import { stream, Handler, HandlerEvent } from '@netlify/functions';
 
-export const handler: Handler = stream(async (event: HandlerEvent, context: HandlerContext) => {
-  // const { command } = context.params;
-  console.log(event);
+export const handler: Handler = stream(async (event: HandlerEvent) => {
+  
   const apiKey = process.env.BGG_API_AUTH_TOKEN;
   const urlSplit = event.rawUrl.split('/.netlify/functions/bgg-api/');
   const apiString = urlSplit[urlSplit.length - 1];
-  // req.url = `https://boardgamegeek.com/xmlapi2/${apiString}`;
-  // req.headers.append('Authorization', `Bearer ${apiKey}`);
+
   const response = await fetch(
     `https://boardgamegeek.com/xmlapi2/${apiString}`, {
     headers: {
@@ -15,17 +13,10 @@ export const handler: Handler = stream(async (event: HandlerEvent, context: Hand
       Authorization: `Bearer ${apiKey}`,
     }},
   );
-  console.log(response);
-  // const text = response.text();
-  // console.log(text);
+
   return {
     headers: {'content-type': 'text/event-stream'},
-    // ...response,
     statusCode: response.status,
     body: response.body,
   };
 });
-
-// export const config: Config = {
-//   path: "/bgg-api/:command"
-// };
